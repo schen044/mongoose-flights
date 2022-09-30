7fdde10cdd8d17f7f50ee1f65a5175614aa9eb70
schen044 <ninjarulers@yahoo.com>
2022-09-30 14:34:56 -0700

    Sets up base database.js configs

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// connect db with mongoose
+require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
